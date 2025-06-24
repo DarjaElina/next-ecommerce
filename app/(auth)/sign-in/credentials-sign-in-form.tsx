d503b6a5bd4e31eb1c80c3cf8be99aa2586aafa2
DarjaElina <daria.elina98@gmail.com>
2025-06-25 00:03:20 +0300

    add sign in logic

--- app/(auth)/sign-in/credentials-sign-in-form.tsx
+++ app/(auth)/sign-in/credentials-sign-in-form.tsx
@@ -5,10 +5,27 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useActionState } from "react";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form action="">
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label className="mb-3" htmlFor="email">
@@ -37,10 +54,13 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
           <Link href="/sign-up" target="_self" className="link">
